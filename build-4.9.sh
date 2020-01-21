3b0d44132606a1561e612e6e96cce2eb4e2057e8
Stefan Rücker <s.ruecker@gmx.de>
2020-01-21 08:59:06 +0100

    Opengapps use master branch ten has been merged back

--- build-4.9.sh
+++ build-4.9.sh
@@ -89,10 +89,10 @@ index 18983252..134ba366 100644
 +  <remote name="gitlab" fetch="https://gitlab.opengapps.org/opengapps/" />
 +
 +  <project path="vendor/opengapps/build" name="aosp_build" revision="master" remote="opengapps" />
-+  <project path="vendor/opengapps/sources/all" name="all" clone-depth="1" revision="ten" remote="gitlab" />
++  <project path="vendor/opengapps/sources/all" name="all" clone-depth="1" revision="master" remote="gitlab" />
 +  <!-- arm64 depends on arm -->
-+  <project path="vendor/opengapps/sources/arm" name="arm" clone-depth="1" revision="ten" remote="gitlab" />
-+  <project path="vendor/opengapps/sources/arm64" name="arm64" clone-depth="1" revision="ten" remote="gitlab" />
++  <project path="vendor/opengapps/sources/arm" name="arm" clone-depth="1" revision="master" remote="gitlab" />
++  <project path="vendor/opengapps/sources/arm64" name="arm64" clone-depth="1" revision="master" remote="gitlab" />
  </manifest>
 EOF
 popd
