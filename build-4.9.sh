fe15a4d0b109a7fac989de1fcc60c5881801d929
Stefan Rücker <s.ruecker@gmx.de>
2020-02-16 00:47:03 +0100

    Remove pick linux 4.9 PR

--- build-4.9.sh
+++ build-4.9.sh
@@ -277,10 +277,6 @@ EOF
         git cherry-pick --no-edit 6f161dcdb89ad62de58d5ec55ed73bd65e03e54d
     popd
 
-    pushd kernel/sony/msm-4.9/kernel
-        _pick_pr sony 2184 3
-    popd
-
     # ----------------------------------------------------------------------
     # Pull opengapps large files that are stored in git lfs
     # ----------------------------------------------------------------------
