5e3900fcb2741523da5a4e1f6dd25f8be578aef7
Stefan Rücker <s.ruecker@gmx.de>
2020-07-19 17:48:59 +0200

    Add 5th param to _pick_pr for excluding commits

--- cherry-pick.sh
+++ cherry-pick.sh
@@ -17,13 +17,20 @@ _pick_pr() {
     local _pr_id=$2
     local _commits=${3:-1}
     local _max_commits=${4:-$_commits}
+    local _excluded_commits=${5:-}
     local _index=$(($_commits - 1))
     local _count=0
+    local _current_commit
 
     git fetch $_remote pull/$_pr_id/head
 
     while [ $_index -ge 0 -a $_count -lt $_max_commits ]; do
-        git cherry-pick -Xtheirs --no-edit FETCH_HEAD~$_index
+        _current_commit=`git log --pretty=format:%h FETCH_HEAD~$_index|head -1`
+        if echo $_excluded_commits | grep $_current_commit; then
+            echo "skip commit"
+        else
+            git cherry-pick --allow-empty -Xtheirs --no-edit FETCH_HEAD~$_index
+        fi
         _index=$(($_index - 1))
         _count=$(($_count + 1))
     done
